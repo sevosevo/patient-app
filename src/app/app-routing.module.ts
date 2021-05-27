c9cb40ec53cc5663110a04e0789e94d1b57ed6d3
Vukasin Sevo <sevo2101s@gmail.com>
2021-05-27 08:51:19 +0200

    Fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './feature';
-import {AddPatientComponent, PatientListComponent} from './feature';
+import {AddPatientComponent, PatientListComponent, HomeComponent} from './feature';
 
 const routes: Routes = [
   {
